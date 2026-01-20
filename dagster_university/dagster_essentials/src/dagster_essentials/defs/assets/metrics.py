26b7cf7ba8de8ea3d50e1273b861f1b3252d612e
PanizAsghari <asghari.pz@gmail.com>
2026-01-20 12:02:47 +0000

    fix

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/metrics.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/metrics.py
@@ -61,7 +61,7 @@ def trips_by_week() -> None:
     sum(total_amount) as total_amount,
     sum(trip_distance) as trip_distance
     from trips
-    group by date_trunc(tpep_pickup_datetime,'week')
+    group period
 
     """
     conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
