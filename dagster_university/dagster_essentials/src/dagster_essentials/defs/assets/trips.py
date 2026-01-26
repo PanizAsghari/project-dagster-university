9263befa627c021bbb1abd5db133baaecf3b6e42
PanizAsghari <asghari.pz@gmail.com>
2026-01-26 11:51:51 +0000

    add partitions

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -1,13 +1,16 @@
 import dagster as dg
 import requests
 from dagster_essentials.defs.assets import constants
-import duckdb
-import os
+from dagster_essentials.defs.partitions import montly_partition
 from dagster_duckdb import DuckDBResource
 
-@dg.asset
-def taxi_trip_file() -> None:
-    month_to_fetch='2023-01'
+
+@dg.asset(
+        partitions_def=montly_partition
+)
+def taxi_trip_file(context: dg.AssetExecutionContext) -> None:
+    partition_date_str = context.partition_key
+    month_to_fetch= partition_date_str[:-3]
     raw_trips= requests.get(
          f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
     )
@@ -23,9 +26,10 @@ def taxi_zones_file() -> None:
 @dg.asset(
     deps= ["taxi_trip_file"]
 )
-def taxi_trips(database: DuckDBResource) -> None:
-    query = """
-        create or replace table trips as (
+def taxi_trips(context: dg.AssetExecutionContext,database: DuckDBResource) -> None:
+    partition_date_str = context.partition_key
+    month_to_fetch= partition_date_str[:-3]
+    create_query=""" create table if not exists as (
           select
             VendorID as vendor_id,
             PULocationID as pickup_zone_id,
@@ -36,13 +40,34 @@ def taxi_trips(database: DuckDBResource) -> None:
             tpep_pickup_datetime as pickup_datetime,
             trip_distance as trip_distance,
             passenger_count as passenger_count,
-            total_amount as total_amount
-          from 'data/raw/taxi_trips_2023-01.parquet'
-        );
+            total_amount as total_amount,
+            '{month_to_fetch}' as partition_date
+          from 'data/raw/taxi_trips_{month_to_fetch}.parquet'
+          where 1=0
+        );"""
+    delete_query="""delete from trips where partition_date='{month_to_fetch}'"""
+    insert_query = f"""
+        insert into trips
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount,
+            '{month_to_fetch}' as partition_date
+          from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}'
+        ;
     """
 
     with database.get_connection() as conn:
-        conn.execute(query)
+        conn.execute(create_query)
+        conn.execute(delete_query)
+        conn.execute(insert_query)
 
 @dg.asset(
     deps=["taxi_zones_file"]
