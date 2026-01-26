2e7ce0f69d7ed5e7e9a43f50d7755e96e4d5f6e3
PanizAsghari <asghari.pz@gmail.com>
2026-01-26 12:49:02 +0000

    s

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -4,7 +4,7 @@ from dagster_essentials.defs.partitions import montly_partition,weekly_partition
 trips_by_week= dg.AssetSelection.assets("trips_by_week")
 trip_update_job=dg.define_asset_job(
     name="trip_update_job",
-    selection=dg.AssetSelection.all() - trips_by_week,
+    selection=dg.AssetSelection.assets(["taxi_trips","taxi_trip_file"]),
     partitions_def=montly_partition
 )
 weekly_trip_update_job=dg.define_asset_job(
--- dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
@@ -1,18 +1,13 @@
-from typing import Union
-
 import dagster as dg
-from dagster_essentials.defs.jobs import trip_update_job,weekly_trip_update_job
+from dagster_essentials.defs.jobs import trip_update_job, weekly_trip_update_job
 
 trip_update_schedule = dg.ScheduleDefinition(
-    name="trip_update_schedule",
-    cron_schedule="0 0 5 * *",
-    job=trip_update_job
-)
-weekly_trip_update_schedule = dg.ScheduleDefinition(
-  cron_schedule="0 0 * * 1",
-  job=weekly_trip_update_job
+  name="trip_update_schedule",
+  cron_schedule="0 0 5 * *",
+  job=trip_update_job,
 )
 
-@dg.schedule(cron_schedule="@daily", target="*")
-def schedules(context: dg.ScheduleEvaluationContext) -> Union[dg.RunRequest, dg.SkipReason]:
-    return dg.SkipReason("Skipping. Change this to return a RunRequest to launch a run.")
+weekly_trip_update_schedule = dg.ScheduleDefinition(
+  cron_schedule="0 0 * * 1",
+  job=weekly_trip_update_job,
+)
