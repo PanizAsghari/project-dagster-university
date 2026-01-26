127cd2bc16b208952dbac416b9e8baa7b24a0dab
PanizAsghari <asghari.pz@gmail.com>
2026-01-26 12:43:28 +0000

    fff

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -1,13 +1,17 @@
 import dagster as dg
 from dagster_essentials.defs.partitions import montly_partition,weekly_partition
 
-
-trips_by_week = dg.AssetSelection.assets("trips_by_week")
-
-
-weekly_update_job = dg.define_asset_job(
-    name="weekly_update_job",
-    partitions_def=weekly_partition,
-    selection=trips_by_week
+trips_by_week= dg.AssetSelection.assets("trips_by_week")
+trip_update_job=dg.define_asset_job(
+    name="trip_update_job",
+    selection=dg.AssetSelection.all() - trips_by_week,
+    partitions_def=montly_partition
 )
-
+weekly_trip_update_job=dg.define_asset_job(
+    name="weekly_trip_update_job",
+    selection=trips_by_week,
+    partitions_def=weekly_partition
+)
+@dg.job
+def jobs():
+    pass
--- dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
@@ -1,11 +1,16 @@
 from typing import Union
 
 import dagster as dg
-from dagster_essentials.defs.jobs import weekly_update_job
+from dagster_essentials.defs.jobs import trip_update_job,weekly_trip_update_job
 
+trip_update_schedule = dg.ScheduleDefinition(
+    name="trip_update_schedule",
+    cron_schedule="0 0 5 * *",
+    job=trip_update_job
+)
 weekly_trip_update_schedule = dg.ScheduleDefinition(
   cron_schedule="0 0 * * 1",
-  job=weekly_update_job
+  job=weekly_trip_update_job
 )
 
 @dg.schedule(cron_schedule="@daily", target="*")
