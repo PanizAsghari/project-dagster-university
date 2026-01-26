f7867aa55be033b6a9c42249d1fb5d59970dffdc
PanizAsghari <asghari.pz@gmail.com>
2026-01-26 12:40:52 +0000

    fix

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -5,11 +5,6 @@ from dagster_essentials.defs.partitions import montly_partition,weekly_partition
 trips_by_week = dg.AssetSelection.assets("trips_by_week")
 
 
-trip_update_job = dg.define_asset_job(
-    name="trip_update_job",
-    partitions_def=montly_partition, # partitions added here
-    selection=dg.AssetSelection.assets("taxi_trip_file", "taxi_trips")
-)
 weekly_update_job = dg.define_asset_job(
     name="weekly_update_job",
     partitions_def=weekly_partition,
