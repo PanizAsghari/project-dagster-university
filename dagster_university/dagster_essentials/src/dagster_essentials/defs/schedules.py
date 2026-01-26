c205d493cdd9e097fe86b92ff77ddb02ebb23a26
PanizAsghari <asghari.pz@gmail.com>
2026-01-26 12:41:51 +0000

    fix

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
@@ -1,13 +1,8 @@
 from typing import Union
 
 import dagster as dg
-from dagster_essentials.defs.jobs import trip_update_job,weekly_update_job
+from dagster_essentials.defs.jobs import weekly_update_job
 
-trip_update_schedule = dg.ScheduleDefinition(
-    name="trip_update_schedule",
-    cron_schedule="0 0 5 * *",
-    job=trip_update_job
-)
 weekly_trip_update_schedule = dg.ScheduleDefinition(
   cron_schedule="0 0 * * 1",
   job=weekly_update_job
